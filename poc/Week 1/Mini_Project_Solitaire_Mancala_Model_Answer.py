81312b2af40b973ba6d7c6ff8773457e0abf4b23
Sam N <samnorling@hotmail.com>
2020-05-24 17:18:56 +1200

    Completed solitaire mancala mini project

--- poc/Week 1/Mini_Project_Solitaire_Mancala_Model_Answer.py
+++ poc/Week 1/Mini_Project_Solitaire_Mancala_Model_Answer.py
@@ -16,7 +16,7 @@ class SolitaireMancala:
         """
         Create Mancala game with empty store and no houses
         """
-        self.board = [0]
+        self._board = [0]
     
     def set_board(self, configuration):
         """
@@ -24,60 +24,60 @@ class SolitaireMancala:
         house zero corresponds to the store and is on right
         houses are number in ascending order from right to left
         """
-        self.board = list(configuration)
+        self._board = list(configuration)
     
     def __str__(self):
         """
         Return string representation for Mancala board
         """
-        board_config = []
-        for house in range(1, len(self.board) + 1):
-            board_config.append(self.board[-house])
-        return str(board_config)
+        temp = list(self._board)
+        temp.reverse()
+        return str(temp)
     
     def get_num_seeds(self, house_num):
         """
         Return the number of seeds in given house on board
         """
-        return self.board[house_num]
+        return self._board[house_num]
 
     def is_game_won(self):
         """
         Check to see if all houses but house zero are empty
         """
-        house_sum = 0
-        for house in range (1, len(self.board)):
-            house_sum += self.board[house]
-        return house_sum == 0
+        for idx in range(1, len(self._board)):
+            if self._board[idx] != 0:
+                return False
+        return True
     
     def is_legal_move(self, house_num):
         """
         Check whether a given move is legal
         """
-        if house_num == 0:
-            return False
-        return self.get_num_seeds(house_num) == house_num
+        move_in_range = 0 < house_num < len(self._board)
+        index_matches = self._board[house_num] == house_num
+        return move_in_range and index_matches
+
     
     def apply_move(self, house_num):
         """
         Move all of the stones from house to lower/left houses
         Last seed must be played in the store (house zero)
         """
-        if self.is_legal_move(house_num):
-            self.board[house_num] = 0
-            for house in range(house_num - 1, -1, -1):
-                self.board[house] += 1
+        if self.is_legal_move(house_num):  
+            for idx in range(house_num):
+                self._board[idx] += 1
+            self._board[house_num] = 0
 
     def choose_move(self):
         """
         Return the house for the next shortest legal move
         Shortest means legal move from house closest to store
-        Note that using a longer legal move would make smaller moves illegal
+        Note that using a longer legal move would make smaller illegal
         If no legal move, return house zero
         """
-        for house in range(1, len(self.board)):
-            if self.is_legal_move(house):
-                return house
+        for house_num in range(1, len(self._board)):
+            if self.is_legal_move(house_num):
+                return house_num
         return 0
     
     def plan_moves(self):
@@ -87,13 +87,15 @@ class SolitaireMancala:
         when given a choice of legal moves
         Not used in GUI version, only for machine testing
         """
-        move_seq = []
-        game_copy = SolitaireMancala()
-        game_copy.set_board(list(self.board))
-        while game_copy.choose_move() > 0:
-            move_seq.append(game_copy.choose_move())
-            game_copy.apply_move(game_copy.choose_move())
-        return move_seq
+        new_board = SolitaireMancala()
+        new_board.set_board(self._board)
+        move_list = []
+        next_move =  new_board.choose_move()
+        while next_move != 0:
+            new_board.apply_move(next_move)
+            move_list.append(next_move)
+            next_move = new_board.choose_move()
+        return move_list
  
 
 # Create tests to check the correctness of your code
@@ -113,14 +115,12 @@ def test_mancala():
     print "Testing get_num_seeds - Computed:", my_game.get_num_seeds(1), "Expected:", config1[1]
     print "Testing get_num_seeds - Computed:", my_game.get_num_seeds(3), "Expected:", config1[3]
     print "Testing get_num_seeds - Computed:", my_game.get_num_seeds(5), "Expected:", config1[5]
-    
+
     # add more tests here
-    
-    print my_game.plan_moves()
-    print "Testing set_board - Computed:", str(my_game), "Expected:", str([0, 5, 3, 1, 1, 0, 0])
     
 test_mancala()
 
+
 # Import GUI code once you feel your code is correct
-import poc_mancala_gui
-poc_mancala_gui.run_gui(SolitaireMancala())
+# import poc_mancala_gui
+# poc_mancala_gui.run_gui(SolitaireMancala())
